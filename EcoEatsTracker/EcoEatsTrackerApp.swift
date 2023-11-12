af34a915caddfd95f49a19df10dd2465ba7e6de5
Noga Gercsak <135066149+nogagercsak@users.noreply.github.com>
2023-11-12 11:41:23 -0500

    Initial Commit

---
+++ EcoEatsTracker/EcoEatsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoEatsTrackerApp.swift
+//  EcoEatsTracker
+//
+//  Created by Noga Gercsak on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EcoEatsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
